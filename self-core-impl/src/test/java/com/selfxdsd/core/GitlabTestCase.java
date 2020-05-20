4a1f7fce1139519fd257655fddf75b6f4954ccbc
cristianpela <cristypela05@gmail.com>
2020-05-20 13:43:39 +0300

    fixed linting errors, that escaped the initial checkstyle audit.

--- self-core-impl/src/test/java/com/selfxdsd/core/GitlabTestCase.java
+++ self-core-impl/src/test/java/com/selfxdsd/core/GitlabTestCase.java
@@ -18,7 +18,7 @@ import static org.mockito.Mockito.when;
  * @version $Id$
  * @since 0.0.1
  */
-final public class GitlabTestCase {
+public final class GitlabTestCase {
 
     /**
      * A repo can be fetched from Gitlab as json.
@@ -28,28 +28,31 @@ final public class GitlabTestCase {
         final Provider gitlab = createGitlab("criske");
         final JsonObject jsonRepo = gitlab.repo("18889648").json();
         assertThat(jsonRepo.getInt("id"), equalTo(18889648));
-        assertThat(jsonRepo.getString("path_with_namespace"), equalTo("criske/test2"));
+        assertThat(jsonRepo.getString("path_with_namespace"),
+                equalTo("criske/test2"));
     }
 
     /**
      * IllegalStateException is thrown because the repo is not found.
      */
-    @Test(expected = IllegalStateException.class )
+    @Test(expected = IllegalStateException.class)
     public void fetchesRepoNotFound() {
         createGitlab("criske").repo("1231312123").json();
     }
 
     /**
-     * Creates a Gitlab instance based on a mocked {@link com.selfxdsd.api.User}
+     * Creates a Gitlab instance based on a mocked
+     * {@link com.selfxdsd.api.User}.
      *
      * @param username Provided user name
      * @return Created Gitlab instance
      */
-    @SuppressWarnings("SameParameterValue")
     private Gitlab createGitlab(final String username) {
         final User user = mock(User.class);
         when(user.username()).thenReturn(username);
-        when(user.provider()).then(invocation -> new Gitlab((User) invocation.getMock(), null));
+        when(user.provider()).then(invocation -> new Gitlab(
+                (User) invocation.getMock(),
+                null));
         return (Gitlab) user.provider();
     }
 
