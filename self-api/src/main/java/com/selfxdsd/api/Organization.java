3d019018af68d00b7766232f88e435ca60843153
cristianpela <cristypela05@gmail.com>
2020-07-03 08:08:04 +0300

    #122 Review fixes.

--- self-api/src/main/java/com/selfxdsd/api/Organization.java
+++ self-api/src/main/java/com/selfxdsd/api/Organization.java
@@ -18,47 +18,14 @@ public interface Organization {
 
     /**
      * Organization repos.
-     * @return OrganizationRepos.
+     * @return Repos.
      */
-    OrganizationRepos repos();
+    Repos repos();
 
     /**
      * The Organization in JSON format as returned by the provider's API.
      * @return JsonObject.
      */
     JsonObject json();
-
-    /**
-     * Extra information about the organization like name,
-     * description etc...
-     * @return Detail
-     */
-    Detail detail();
-
-    /**
-     * Contains extra information about the organization like name,
-     * description etc...
-     */
-    interface Detail {
-
-        /**
-         * Organization name.
-         * @return String
-         */
-        String name();
-
-        /**
-         * Organization description.
-         * @return String.
-         */
-        String description();
-
-        /**
-         * The Organization extra detail in JSON format as returned by
-         * the provider's API.
-         * @return JsonObject.
-         */
-        JsonObject json();
-    }
 
 }
---
+++ self-api/src/main/java/com/selfxdsd/api/Repos.java
@@ -0,0 +1,10 @@
+package com.selfxdsd.api;
+
+/**
+ * Provider repos.
+ * @author criske
+ * @version $Id$
+ * @since 0.0.8
+ */
+public interface Repos extends Iterable<Repo> {
+}
