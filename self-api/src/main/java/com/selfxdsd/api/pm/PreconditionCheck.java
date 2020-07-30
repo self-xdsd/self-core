47234db8314bebe6d9865443d6080b972f29371f
amihaiemil <amihaiemil@gmail.com>
2020-07-30 13:28:37 +0300

    #324 pm Step, Intermediary and PreconditionCheck

---
+++ self-api/src/main/java/com/selfxdsd/api/pm/PreconditionCheck.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2020, Self XDSD Contributors
+ * All rights reserved.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to read the Software only. Permission is hereby NOT GRANTED to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software.
+ * <p>
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.selfxdsd.api.pm;
+
+/**
+ * A step which validates a condition and splits the path
+ * in two. One in case the condition is met, one in case the
+ * condition is not met.
+ * @author Mihai Andronache (amihaiemil@gmail.com)
+ * @version $Id$
+ * @since 0.0.13
+ */
+public abstract class PreconditionCheck implements Step {
+    /**
+     * Next step if the check is true.
+     */
+    private Step onTrue;
+
+    /**
+     * Next step if the check is false.
+     */
+    private Step onFalse;
+
+    /**
+     * Ctor.
+     * @param onTrue Step that should be performed next if the check is true.
+     * @param onFalse Step that should be performed next if the check is false.
+     */
+    public PreconditionCheck(
+        final Step onTrue,
+        final Step onFalse
+    ) {
+        this.onTrue = onTrue;
+        this.onFalse = onFalse;
+    }
+
+    /**
+     * Step to perform on successful check.
+     * @return Step
+     */
+    public final Step onTrue() {
+        return this.onTrue;
+    }
+
+    /**
+     * Step to perform on failed check.
+     * @return Step
+     */
+    public final Step onFalse() {
+        return this.onFalse;
+    }
+}
