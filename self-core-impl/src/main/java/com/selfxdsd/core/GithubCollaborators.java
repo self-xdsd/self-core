f6ff876d1173b3c0b0e6372d509e4e35b398d9b3
amihaiemil <amihaiemil@gmail.com>
2020-07-31 14:32:33 +0300

    #331 GithubCollaborators implemented + logging with SLF4J

--- self-core-impl/src/main/java/com/selfxdsd/core/GithubCollaborators.java
+++ self-core-impl/src/main/java/com/selfxdsd/core/GithubCollaborators.java
@@ -24,7 +24,11 @@ package com.selfxdsd.core;
 
 import com.selfxdsd.api.Collaborators;
 import com.selfxdsd.api.storage.Storage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.json.Json;
+import java.net.HttpURLConnection;
 import java.net.URI;
 
 /**
@@ -36,6 +40,13 @@ import java.net.URI;
  *  Don't forget about unit tests.
  */
 final class GithubCollaborators implements Collaborators {
+
+    /**
+     * Logger.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(
+        GithubCollaborators.class
+    );
 
     /**
      * Github repo Collaborators base uri.
@@ -71,6 +82,30 @@ final class GithubCollaborators implements Collaborators {
 
     @Override
     public boolean invite(final String username) {
-        return false;
+        final boolean result;
+        LOG.debug(
+            "Inviting user " + username
+            + " to [" + this.collaboratorsUri.toString() + "]."
+        );
+        final Resource response = this.resources.put(
+            URI.create(this.collaboratorsUri.toString() + "/" + username),
+            Json.createObjectBuilder().build()
+        );
+        if(response.statusCode() == HttpURLConnection.HTTP_CREATED) {
+            result = true;
+            LOG.debug("Invitation successfully created!");
+        } else if (response.statusCode() == HttpURLConnection.HTTP_NO_CONTENT) {
+            result = true;
+            LOG.debug("User was already invited, everything is ok.");
+        } else {
+            result = false;
+            LOG.error(
+                "Unexpected status when inviting user " + username
+                + " to [" + this.collaboratorsUri.toString() + "]. "
+                + "Expected 201 CREATED or 204 NO CONTENT, but got "
+                + response.statusCode()
+            );
+        }
+        return result;
     }
 }
