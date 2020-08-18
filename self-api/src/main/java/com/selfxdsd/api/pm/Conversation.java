d2dc8f60f292de7f4370b508762a1b46afdedf77
amihaiemil <amihaiemil@gmail.com>
2020-08-18 20:49:40 +0300

    #441 sketch of Conversation + puzzle

---
+++ self-api/src/main/java/com/selfxdsd/api/pm/Conversation.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2020, Self XDSD Contributors
+ * All rights reserved.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to read the Software only. Permission is hereby NOT GRANTED to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software.
+ * <p>
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.selfxdsd.api.pm;
+
+import com.selfxdsd.api.Event;
+
+/**
+ * Conversation that the PM has through Issue comments.
+ * @author Mihai Andronache (amihaiemil@gmail.com)
+ * @version $Id$
+ * @since 0.0.20
+ * @todo #441:60min Add the first implementation of Conversation,
+ *  which should be named Understand and should use the project's
+ *  Language to understand the comment and set the event's type
+ *  accordingly.
+ */
+public interface Conversation {
+
+    /**
+     * Start the conversation, figure out what steps
+     * need to be executed in order to fulfil the received
+     * comment.
+     * @param event Event which started the conversation. It should
+     *  contain a Comment.
+     * @return Step. The step(s) which need to be performed as a follow-up
+     *  of this conversation.
+     */
+    Step start(final Event event);
+
+}
