53c9fc79a1c370c5e45b4bc45909a07059ec104a
amihaiemil <amihaiemil@gmail.com>
2020-08-20 11:10:18 +0300

    459 UnassignTask step implemented and tested

---
+++ self-core-impl/src/main/java/com/selfxdsd/core/managers/UnassignTask.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2020, Self XDSD Contributors
+ * All rights reserved.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to read the Software only. Permission is hereby NOT GRANTED to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software.
+ * <p>
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.selfxdsd.core.managers;
+
+import com.selfxdsd.api.Event;
+import com.selfxdsd.api.Project;
+import com.selfxdsd.api.Task;
+import com.selfxdsd.api.pm.Intermediary;
+import com.selfxdsd.api.pm.Step;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Step which unassigns a task.
+ * @author Mihai Andronache (amihaiemil@gmail.com)
+ * @version $Id$
+ * @since 0.0.20
+ */
+public final class UnassignTask extends Intermediary {
+
+    /**
+     * Logger.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(
+        UnassignTask.class
+    );
+
+    /**
+     * Ctor.
+     *
+     * @param next The next step to perform.
+     */
+    public UnassignTask(final Step next) {
+        super(next);
+    }
+
+    @Override
+    public void perform(final Event event) {
+        final String issueId = event.issue().issueId();
+        final Project project = event.project();
+        final Task task = project.tasks().getById(
+            issueId, project.repoFullName(), project.provider()
+        );
+        if(task == null || task.assignee() == null) {
+            LOG.debug(
+                "Task #" + issueId + " is not register or it is not assigned, "
+                + "nothing to do.");
+        } else {
+            LOG.debug(
+                "Resigning @" + task.assignee().username() + " from task "
+                + "#" + issueId + " of project " + project.repoFullName()
+                + " at " + project.provider()
+            );
+            final Task unassigned = task.unassign();
+            if(unassigned.assignee() == null) {
+                LOG.debug("Resignation successful!");
+            } else {
+                LOG.debug(
+                    "Something went wrong, the task is still assigned to "
+                    + unassigned.assignee().username()
+                );
+            }
+        }
+        this.next().perform(event);
+    }
+}
