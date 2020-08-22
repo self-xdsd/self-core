605a79b5e2f9a5b395f3af0408e5e168a2baf5a4
cristianpela <cristypela05@gmail.com>
2020-08-22 14:27:59 +0300

    #473. StepComposer implemented+tested.
    - support for PreConditionCheck.

--- self-core-impl/src/main/java/com/selfxdsd/core/managers/StepComposer.java
+++ self-core-impl/src/main/java/com/selfxdsd/core/managers/StepComposer.java
@@ -4,11 +4,16 @@ import com.selfxdsd.api.pm.Step;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * Helper class that allows chaining {@link com.selfxdsd.api.pm.Intermediary}
  * steps.
+ * <br/>
+ * It also provides composition support for
+ * {@link com.selfxdsd.api.pm.PreconditionCheck}.
  * @author criske
  * @version $Id$
  * @since 0.0.20
@@ -32,6 +37,20 @@ public final class StepComposer {
     }
 
     /**
+     * Stars a ConditionComposer.
+     * @param when BiFunction providing onTrue, onFalse. Returning should be a
+     *             {@link com.selfxdsd.api.pm.PreconditionCheck}.
+     * @return ConditionComposer.
+     */
+    public ConditionComposer when(final BiFunction<Step, Step, Step> when) {
+        if(!callStack.isEmpty()){
+            throw new IllegalStateException("Condition composition can't be "
+                + " called after an next() call!");
+        }
+        return new ConditionComposer(when);
+    }
+
+    /**
      * Finishes the chain into one step.
      * @param lastStep Last Step.
      * @return Composed Step.
@@ -50,6 +69,72 @@ public final class StepComposer {
      */
     public Step finish(){
         return finish(lastly -> {});
+    }
+
+
+    /**
+     * StepComposer for a PreconditionCheck.
+     */
+    public static final class ConditionComposer {
+
+        /**
+         * BiFunction providing onTrue, onFalse steps. Returning should be a
+         * PreconditionCheck.
+         */
+        private final BiFunction<Step, Step, Step> when;
+
+        /**
+         * OnTrue Step supplier.
+         */
+        private Supplier<Step> onTrue;
+
+        /**
+         * OnFalse Step supplier.
+         */
+        private Supplier<Step> onFalse;
+
+        /**
+         * Private constructor.
+         * @param when BiFunction providing onTrue, onFalse. Returning
+         *             should be a PreconditionCheck.
+         */
+        private ConditionComposer(final BiFunction<Step, Step, Step> when) {
+            this.when = when;
+        }
+
+        /**
+         * OnTrue setter.
+         * @param onTrue OnTrue Step supplier.
+         * @return This ConditionComposer.
+         */
+        public ConditionComposer onTrue(final Supplier<Step> onTrue){
+            this.onTrue = onTrue;
+            return this;
+        }
+
+        /**
+         * OnFalse setter.
+         * @param onFalse OnFalse Step supplier.
+         * @return This ConditionComposer.
+         */
+        public ConditionComposer onFalse(final Supplier<Step> onFalse){
+            this.onFalse = onFalse;
+            return this;
+        }
+
+        /**
+         * Finishes the composition into a composed Step provided by `when`.
+         * @return Step.
+         */
+        public Step finish(){
+            if (onFalse == null) {
+                onFalse = () -> e -> { };
+            }
+            if (onTrue == null) {
+                onTrue = () -> e -> { };
+            }
+            return when.apply(onTrue.get(), onFalse.get());
+        }
     }
 
 }
--- self-core-impl/src/test/java/com/selfxdsd/core/managers/StepComposerTestCase.java
+++ self-core-impl/src/test/java/com/selfxdsd/core/managers/StepComposerTestCase.java
@@ -46,4 +46,62 @@ public final class StepComposerTestCase {
         MatcherAssert.assertThat(order, Matchers.equalTo("12345"));
     }
 
+    /**
+     * Can perform conditional composition on true branch.
+     */
+    @Test
+    public void shouldTakeTrueConditionBranch(){
+        final StringBuilder builder = new StringBuilder();
+        final AtomicInteger counter = new AtomicInteger();
+        new StepComposer()
+            .when((tru, fls) -> e -> {
+                builder.append("true");
+                tru.perform(e);
+            })
+            .onTrue(() -> new StepComposer()
+                .next(s -> e -> {
+                    builder.append(counter.incrementAndGet());
+                    s.perform(e);
+                })
+                .next(s -> e -> {
+                    builder.append(counter.incrementAndGet());
+                    s.perform(e);
+                })
+                .finish())
+            .finish()
+            .perform(Mockito.mock(Event.class));
+
+        final String order = builder.toString();
+        MatcherAssert.assertThat(order, Matchers.equalTo("true12"));
+    }
+
+    /**
+     * Can perform conditional composition on false branch.
+     */
+    @Test
+    public void shouldTakeFalseConditionBranch(){
+        final StringBuilder builder = new StringBuilder();
+        final AtomicInteger counter = new AtomicInteger();
+        new StepComposer()
+            .when((tru, fls) -> e -> {
+                builder.append("false");
+                fls.perform(e);
+            })
+            .onFalse(() -> new StepComposer()
+                .next(s -> e -> {
+                    builder.append(counter.incrementAndGet());
+                    s.perform(e);
+                })
+                .next(s -> e -> {
+                    builder.append(counter.incrementAndGet());
+                    s.perform(e);
+                })
+                .finish())
+            .finish()
+            .perform(Mockito.mock(Event.class));
+
+        final String order = builder.toString();
+        MatcherAssert.assertThat(order, Matchers.equalTo("false12"));
+    }
+
 }
