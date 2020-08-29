37cd64bc98aac22290354cab5f25a8cab51e573a
amihaiemil <amihaiemil@gmail.com>
2020-08-29 16:02:14 +0300

    #462 Resignations scaffolding

---
+++ self-api/src/main/java/com/selfxdsd/api/Resignations.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2020, Self XDSD Contributors
+ * All rights reserved.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to read the Software only. Permission is hereby NOT GRANTED to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software.
+ * <p>
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.selfxdsd.api;
+
+/**
+ * Task resignations.
+ * @author Mihai Andronache (amihaiemil@gmail.com)
+ * @version $Id$
+ * @since 0.0.21
+ */
+public interface Resignations extends Iterable<Resignation> {
+
+    /**
+     * Get a Task's resignations.
+     * @param task Task in question.
+     * @return Resignations of the given Task.
+     */
+    Resignations ofTask(final Task task);
+
+    /**
+     * Possible reasons for resignation.
+     */
+    class Reason {
+
+        /**
+         * Hidden ctor.
+         */
+        private Reason(){ }
+
+        /**
+         * The contributor asked to be resigned.
+         */
+        public static final String ASKED = "ASKED";
+
+        /**
+         * The deadline was not met.
+         */
+        public static final String DEADLINE = "DEADLINE";
+    }
+}
