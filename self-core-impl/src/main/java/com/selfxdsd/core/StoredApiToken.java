d2ae2ecd8ec63e511f683f8d8d596bdc05a7b645
andreoss <andreoss@sdf.org>
2021-02-02 04:22:40 -0500

    (#947) Add license & add Storage as parameter

--- self-core-impl/src/main/java/com/selfxdsd/core/StoredApiToken.java
+++ self-core-impl/src/main/java/com/selfxdsd/core/StoredApiToken.java
@@ -1,14 +1,46 @@
-package com.selfxdsd.core;
+/**
+ * Copyright (c) 2020-2021, Self XDSD Contributors
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to read the Software only. Permission is hereby NOT GRANTED to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */package com.selfxdsd.core;
 
 import com.selfxdsd.api.ApiToken;
+import com.selfxdsd.api.storage.Storage;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Objects;
 
 /**
  * Stored API Token.
+ *
+ * @author Andrei Osipov (andreoss@sdf.org)
+ * @version $Id$
+ * @since 0.0.61
  */
 public final class StoredApiToken implements ApiToken {
+
+    /**
+     * The storage.
+     */
+    private final Storage storage;
+
     /**
      * Token's name.
      */
@@ -32,10 +64,12 @@ public final class StoredApiToken implements ApiToken {
      * @param expiration Expiration date.
      */
     public StoredApiToken(
+        final Storage storage,
         final String name,
         final byte[] secret,
         final LocalDateTime expiration
     ) {
+        this.storage = storage;
         this.name = name;
         this.secret = secret;
         this.expiration = expiration;
--- self-core-impl/src/test/java/com/selfxdsd/core/StoredApiTokenTestCase.java
+++ self-core-impl/src/test/java/com/selfxdsd/core/StoredApiTokenTestCase.java
@@ -1,11 +1,35 @@
+/**
+ * Copyright (c) 2020-2021, Self XDSD Contributors
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to read the Software only. Permission is hereby NOT GRANTED to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.selfxdsd.core;
 
 import com.selfxdsd.api.ApiToken;
+import com.selfxdsd.api.storage.Storage;
 import java.time.LocalDateTime;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 /**
  * Unit tests for {@link StoredApiToken}.
@@ -18,6 +42,7 @@ public final class StoredApiTokenTestCase {
     public void returnsName() {
         MatcherAssert.assertThat(
             new StoredApiToken(
+                Mockito.mock(Storage.class),
                 "gh",
                 new byte[]{'p', 'a', 's', 's'},
                 LocalDateTime.MAX
@@ -33,6 +58,7 @@ public final class StoredApiTokenTestCase {
     public void returnsExpirationDate() {
         MatcherAssert.assertThat(
             new StoredApiToken(
+                Mockito.mock(Storage.class),
                 "gh",
                 new byte[]{'p', 'a', 's', 's'},
                 LocalDateTime.MAX
@@ -49,6 +75,7 @@ public final class StoredApiTokenTestCase {
         Assert.assertArrayEquals(
             new byte[]{'p', 'a', 's', 's'},
             new StoredApiToken(
+                Mockito.mock(Storage.class),
                 "gh",
                 new byte[]{'p', 'a', 's', 's'},
                 LocalDateTime.MAX
@@ -62,11 +89,13 @@ public final class StoredApiTokenTestCase {
     @Test
     public void comparesStoredUserObjects() {
         final ApiToken first = new StoredApiToken(
+            Mockito.mock(Storage.class),
             "user",
             new byte[]{'p', 'a', 's', 's'},
             LocalDateTime.MAX
         );
         final ApiToken second = new StoredApiToken(
+            Mockito.mock(Storage.class),
             "user",
             new byte[]{'p', 'a', 's', 's'},
             LocalDateTime.MAX
@@ -87,11 +116,13 @@ public final class StoredApiTokenTestCase {
     @Test
     public void comparesStoredUserObjectsNegative() {
         final ApiToken first = new StoredApiToken(
+            Mockito.mock(Storage.class),
             "user",
             new byte[]{'p', 'a', 's', 's'},
             LocalDateTime.MAX
         );
         final ApiToken second = new StoredApiToken(
+            Mockito.mock(Storage.class),
             "user",
             new byte[]{'p', 'a', 'x', 'x'},
             LocalDateTime.MAX
