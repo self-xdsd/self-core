693ce998615cd0af03e62f3cdfaea37a0ea446a1
alilosoft <fellahi.ali@gmail.com>
2021-03-02 18:08:32 +0100

    #979 Add Payments interface

---
+++ self-api/src/main/java/com/selfxdsd/api/Payments.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2020-2021, Self XDSD Contributors
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to read the Software only. Permission is hereby NOT GRANTED to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.selfxdsd.api;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * Represent all the payments done for an {@link Invoice}.
+ * <br>
+ * All failed and the last successful one.
+ * @author Ali Fellahi (fellahi.ali@gmail.com)
+ * @version $Id$
+ * @since 0.0.67
+ */
+public interface Payments extends Iterable<Payment> {
+
+    /**
+     * Register a new {@link Payment}.
+     * @param invoice Invoice to be payed.
+     * @param transactionId the transaction id of successful payment.
+     * @param timestamp payment time.
+     * @param value amount payed.
+     * @param status SUCCESSFUL or FAILED.
+     * @param failReason why the payment has failed if any.
+     * @return registered Payment.
+     */
+    Payment register(
+        final Invoice invoice,
+        final String transactionId,
+        final LocalDateTime timestamp,
+        final BigDecimal value,
+        final String status,
+        final String failReason
+    );
+
+    /**
+     * Get all the Payments of a given {@link Invoice}.
+     * @param invoice {@link Invoice}.
+     * @return Payments.
+     */
+    Payments ofInvoice(final Invoice invoice);
+}
