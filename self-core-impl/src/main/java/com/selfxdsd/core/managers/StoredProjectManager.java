2957156ac527b01f0754342fb454991cb6c9a336
Mihai <amihaiemil@gmail.com>
2021-10-08 12:07:32 +0300

    removed solved puzzle

--- self-core-impl/src/main/java/com/selfxdsd/core/managers/StoredProjectManager.java
+++ self-core-impl/src/main/java/com/selfxdsd/core/managers/StoredProjectManager.java
@@ -49,8 +49,6 @@ import java.util.function.Supplier;
  * @checkstyle ClassFanOutComplexity (1000 lines)
  * @checkstyle ClassDataAbstractionCoupling (1000 lines)
  * @since 0.0.1
- * @todo #1228:60min Update Task/Tasks api to support updating
- *  task estimation.
  * @todo #1228:30min Handle updating task estimation
  *  by resolving Event.Type.TASK_ESTIMATION event when an estimation
  *  label is added or removed. If more estimation labels are found, the biggest
